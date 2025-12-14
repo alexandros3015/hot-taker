164be7141f24572ec534bdc0c20b45eb88832c74
alexandros3015 <alexandrosbeachtoys@gmail.com>
2025-12-14 13:57:01 -0500

    Added better support for images

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
 import { Flame, Book, ThumbsDown, ThumbsUp } from 'lucide-react';
-import Image from  'next/image';
+import Image from 'next/image';
+
+function TakeImage({ src }: { src?: string }) {
+	if (!src) return null;
+	// TODO: Make the formatting work better. Do som experimenting and figure out what works best. PLEASE
+	else if (src !== '') {
+		return (
+			<div className="relative mb-4 h-30 w-2/5 overflow-hidden rounded-lg">
+				<Image
+					src={src}
+					alt="image"
+					fill
+					sizes="(max-width: 768px) 100vw, 40vw"
+					quality={90}
+					className="object-cover"
+				/>
+			</div>
+		);
+	}
+}
 
 export default function Home() {
 	const hotTakes = [
@@ -12,28 +31,54 @@ export default function Home() {
 			agree: 999,
 			disagree: 0,
 			spice_level: 0.5
+		},
+		{
+			id: 1,
+			image: '',
+			author: 'Sparsha',
+			text: '1+1=2',
+			agree: 5,
+			disagree: 2,
+			spice_level: 0.75
 		}
 	];
 
 	return (
 		<ul>
-			{hotTakes.map((value, index) => (
-				<li className="bg-foreground flex w-full flex-col rounded-3xl p-2.5 shadow-lg space-y-4" key={value.id}>
+			{hotTakes.map((value) => (
+				<li
+					className="bg-foreground mb-4 flex w-full flex-col space-y-4 rounded-3xl p-2.5 shadow-lg"
+					key={value.id}
+				>
 					<div className="flex gap-4">
-					  <div className='relative mb-4 overflow-hidden rounded-lg w-2/5'>
-              <Image src={value.image} alt="image" width={800} height={600} sizes='(max-width: 768px) 100vw, 40vw' quality={90} />
+						<TakeImage src={value.image} />
+						<div className="flex flex-col justify-center space-y-1 text-sm">
+							<div className="flex items-center gap-1">
+								<Book size={18} />
+								<strong>Author: </strong>
+								{value.author}
+							</div>
+							<div className="flex items-center gap-1">
+								<Flame size={18} />
+								<strong>Spice Level: </strong>
+								{value.spice_level}
+							</div>
+							<div className="flex items-center gap-1">
+								<ThumbsUp size={18} />
+								<strong>Agrees: </strong>
+								{value.agree}
+							</div>
+							<div className="flex items-center gap-1">
+								<ThumbsDown size={18} />
+								<strong>Disagrees: </strong>
+								{value.disagree}
+							</div>
 						</div>
-              <div className="space-y-1 text-sm flex flex-col justify-center">
-                <div className="flex items-center gap-1"><Book size={18} /><strong>Author: </strong>{value.author}</div>
-                <div className="flex items-center gap-1"><Flame size={18} /><strong>Spice Level: </strong>{value.spice_level}</div>
-                <div className="flex items-center gap-1"><ThumbsUp size={18} /><strong>Agrees: </strong>{value.agree}</div>
-                <div className="flex items-center gap-1"><ThumbsDown size={18} /><strong>Disagrees: </strong>{value.disagree}</div>
-              </div>
-  					</div>
-  					
-  					<div className="p-4 bg-gray-700 rounded-lg">
-              <p>{value.text}</p>
-  					</div>
+					</div>
+
+					<div className="rounded-lg bg-gray-700 p-4">
+						<p>{value.text}</p>
+					</div>
 				</li>
 			))}
 		</ul>
